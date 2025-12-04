86610a89ec28acdd041c8221e5e93c3f1f10f324
Person-0 <atulyajain28122007@gmail.com>
2025-12-04 21:13:32 +0530

    added save and load methods

--- src/knn.ts
+++ src/knn.ts
@@ -11,9 +11,31 @@ export default class KNNCharacterDetector {
     this.classifier = knnClassifier.create();
   }
 
+  getSaveFile() {
+    const data: { [label: string]: tf.Tensor2D | string } = this.classifier.getClassifierDataset();
+    for (const label of Object.keys(data)) {
+      const t = data[label] as tf.Tensor2D;
+      data[label] = JSON.stringify({
+        shape: t.shape,
+        dtype: t.dtype,
+        data: Array.from(t.dataSync())
+      });
+    }
+    return JSON.stringify(data);
+  }
+
+  loadFromData(data: string) {
+    const loaded = JSON.parse(data);
+    for (const label of Object.keys(loaded)) {
+      const parsed = JSON.parse(loaded[label]);
+      loaded[label] = tf.tensor(parsed.data, parsed.shape, parsed.dtype);
+    }
+    this.classifier.setClassifierDataset(loaded);
+  }
+
   imgDataToTensor(tensorData: ImageData) {
     const computed: number[] = [];
-    for(let i = 0; i < tensorData.data.length; i+=4){
+    for (let i = 0; i < tensorData.data.length; i += 4) {
       computed.push(tensorData.data[i]);
     }
     return tf.tensor1d(computed);
